55cea980f3ad33c98a164dbd9c6a1809f6cf0ecb
Nick Johnson <nlj222@gmail.com>
2023-03-22 19:17:46 -0700

    lab complete

--- server/app.py
+++ server/app.py
@@ -16,18 +16,45 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+        if username and password:
+            user = User(username=username)
+            user.password_hash = password
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return user.to_dict(), 201
+        return {'error': '422 Unprocessable Entity'}, 422
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user_id = session['user_id']
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+        user = User.query.filter(User.username == username).first()
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
